4f6758ae1e9ad91f93734ef96680bc230c543e68
LIN GUAN YU <kib7410@gmail.com>
2021-10-06 00:21:28 +0800

    '修改index.php'

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use Demo\Hello;
 
 $world = new World();
 $lara = new Lara();
-$vincent = new Hello\Someome('Vincent');
+$vincent = new Demo\Hello\Someome('Vincent');
 
 $mary = new Demo\Hello\Someome('Mary');
 $john = new Demo\Hello\Someome('John');
