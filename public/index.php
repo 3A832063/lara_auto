88fc51bb77d80f1e94e3201da40439fdd76bdd6a
LIN GUAN YU <kib7410@gmail.com>
2021-10-06 00:15:46 +0800

    '新增index.php'

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+require '../vendor/autoload.php'
+
+use \Demo\HelloWorld as World;
+use Demo\Hello\Lara;
+use Demo\Hello;
+
+$world = new World();
+$lara = new Lara();
+$vincent = new Hello\Someome('Vincent');
+
+$mary = new Demo\Hello\Someome('Mary');
+$john = new Demo\Hello\Someome('John');
+?>
